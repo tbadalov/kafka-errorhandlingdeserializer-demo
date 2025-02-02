7770d4e74f6a67863ffbf7901bcf6e9fb2487b14
Turkhan Badalov <badalov.turxan@gmail.com>
2025-02-02 19:24:30 +0400

    Simple string kafka consumer

---
+++ src/main/java/com/example/kafkaconsumer/EventListener.java
@@ -0,0 +1,15 @@
+package com.example.kafkaconsumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventListener {
+
+    @KafkaListener(topics = "sms-topic")
+    public void listen(ConsumerRecord<String, String> sms) {
+        System.out.println("Received sms: " + sms.value());
+    }
+
+}
